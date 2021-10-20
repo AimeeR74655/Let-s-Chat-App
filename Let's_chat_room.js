22be23fad617b5705b10d571bf48055847391649
AimeeR74655 <82007449+AimeeR74655@users.noreply.github.com>
2021-10-19 23:11:23 -0400

    Add files via upload

---
+++ Let's_chat_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAfSE_PH5XnemaKyJ8d0jtW2zRgL-Cpqbk",
+    authDomain: "let-s-chat-f5eec.firebaseapp.com",
+    projectId: "let-s-chat-f5eec",
+    storageBucket: "let-s-chat-f5eec.appspot.com",
+    messagingSenderId: "232842524014",
+    appId: "1:232842524014:web:cd3052ca51e9408fa9fd7f"
+  };
+  
+  // Initialize Firebase
+   initializeApp(firebaseConfig);
